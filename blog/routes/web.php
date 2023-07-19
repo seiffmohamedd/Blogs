599dc028333edb5d35946573030481440f084eea
seiffmohamedd <seif500mohamed@gmail.com>
2023-07-19 10:34:55 +0300

    routing encap

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Models\Post;
-
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,14 +24,27 @@ Route::get('/hello',function(){
 
 Route::get('/post/{post}',function($postnum){
 
-    $post =Post::find($postnum); 
+    $fileDir=__DIR__ . "/../resources/posts/{$postnum}.html";
 
-    return view('posts', [
-        'post' => $post
+    if( ! file_exists($fileDir))
+    {
+        // dd("file does not exist");
+        //or use 404
+        abort(404);
+        //or use redirect to home page
+        // return redirect('/');
+    }
+
+
+    $post = cache()->remember("posts.{$postnum}",5 , function() use ($fileDir){ //now()->addMinutes() we feeh addHours() bdal ma tekteb secs fel params
+        var_dump('file_get_contents');
+        return file_get_contents($fileDir);
+    });
+
+  
+    return view('post',[
+        'post'=> $post
     ]);
-
-
-
 })-> where('post','[A-z_\-]+');
 
 //u can use multiple methods whereAlphaNumeric means it allows alphabetic and nums 
