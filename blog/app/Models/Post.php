c109b1efddb6d0507362b49089ec590cd0496727
seiffmohamedd <seif500mohamed@gmail.com>
2023-07-19 14:10:10 +0300

    first 2 sections are done

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -1,28 +1,42 @@
 <?php
 namespace App\Models;
 
-class Post{
-    public static function find($slug){
-        
-    $fileDir=__DIR__ . "/../resources/posts/{$postnum}.html";
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
 
-    if( ! file_exists($fileDir))
+use Closure;
+
+use Illuminate\Support\Facades\Factory;
+
+
+
+class Post
+{
+
+    public static function all()
     {
+        $files = File::files(resource_path("posts/"));
+        // return array_map(function ($file) {
+        //     return $file->get(); // Use get() to fetch the content of the file
+        // }, $files);
+    }
+    
+
+
+    public static function find($postnum)
+    {
+    if( ! file_exists($fileDir = resource_path("posts/{$postnum}.html")))
+        {
         // dd("file does not exist");
         //or use 404
-
-        abort(404);
-
+        // abort(404);
         //or use redirect to home page
-
         // return redirect('/');
-    }
+        // or use model not found built in template
+            throw new ModelNotFoundException();
+        }
 
-    $post= file_get_contents($fileDir);
-
-  
-    return view('post',[
-        'post'=> $post
-    ]);
+        return cache()->remember("posts.{$postnum}",5 , fn() => file_get_contents($fileDir)); //now()->addMinutes() we feeh addHours() bdal ma tekteb secs fel params
+            
     }
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,38 +14,49 @@ use App\Models\Post;
 |
 */
 
-Route::get('/', function () {
-    return view('posts');
+// Route::get('/', function () {
+//     return view('posts');
+// });
+
+
+
+Route::get('/',function(){
+
+
+    return view('posts' , [
+        'posts' => Post::all() 
+
+    ]);
 });
 
-Route::get('/hello',function(){
+    Route::get('/post/{post}',function($postnum){
 
-    return "Hello world!";
-}); 
-
-Route::get('/post/{post}',function($postnum){
-
-    $fileDir=__DIR__ . "/../resources/posts/{$postnum}.html";
-
-    if( ! file_exists($fileDir))
-    {
-        // dd("file does not exist");
-        //or use 404
-        abort(404);
-        //or use redirect to home page
-        // return redirect('/');
-    }
-
-
-    $post = cache()->remember("posts.{$postnum}",5 , function() use ($fileDir){ //now()->addMinutes() we feeh addHours() bdal ma tekteb secs fel params
-        var_dump('file_get_contents');
-        return file_get_contents($fileDir);
-    });
-
-  
-    return view('post',[
-        'post'=> $post
-    ]);
+        return view('post' , [
+            'post' => Post::find($postnum) 
+    
+        ]);
 })-> where('post','[A-z_\-]+');
 
 //u can use multiple methods whereAlphaNumeric means it allows alphabetic and nums 
+
+    // $fileDir=__DIR__ . "/../resources/posts/{$postnum}.html";
+
+    // if( ! file_exists($fileDir))
+    // {
+    //     // dd("file does not exist");
+    //     //or use 404
+    //     abort(404);
+    //     //or use redirect to home page
+    //     // return redirect('/');
+    // }
+
+
+    // $post = cache()->remember("posts.{$postnum}",5 , function() use ($fileDir){ //now()->addMinutes() we feeh addHours() bdal ma tekteb secs fel params
+    //     var_dump('file_get_contents');
+    //     return file_get_contents($fileDir);
+    // });
+
+  
+    // return view('post',[
+    //     'post'=> $post
+    // ]);
