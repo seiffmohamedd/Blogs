d35a8158d363e25c5367c4e1921cfe3cb25f1f60
seiffmohamedd <seif500mohamed@gmail.com>
2023-07-25 18:15:51 +0300

    posts view separated taken from db

--- blog/resources/views/layout.blade.php
+++ blog/resources/views/layout.blade.php
@@ -24,7 +24,7 @@
         <nav class="md:flex md:justify-between md:items-center">
             <div>
                 <a href="/">
-                    <img src="./images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                    <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
                 </a>
             </div>
 
@@ -50,7 +50,7 @@
                     <form method="POST" action="#" class="lg:flex text-sm">
                         <div class="lg:py-3 lg:px-5 flex items-center">
                             <label for="email" class="hidden lg:inline-block">
-                                <img src="./images/mailbox-icon.svg" alt="mailbox letter">
+                                <img src="/images/mailbox-icon.svg" alt="mailbox letter">
                             </label>
 
                             <input id="email" type="text" placeholder="Your email address"
