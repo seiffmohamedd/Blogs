4ac4e611a6dbcb3fc5c55e61ee71148974dfd0e5
seiffmohamedd <seif500mohamed@gmail.com>
2023-07-25 15:04:08 +0300

    images and styling added

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,25 +1,44 @@
 @extends('layout')
 
 @section('content')
-    <h1>Welcome to the blade</h1>
+{{--  --}}
+
+    @include('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+        @if($posts->count() == 0)
+            <p class="text-center">There is no posts</p>
+
+        @else   
+        <x-post-featured-card :post="$posts[0] "/>
+        <div class="lg:grid lg:grid-cols-2">
+        @foreach ($posts->skip(1) as $post)
+        <x-post-card :post="$post" class="bg-red-500"/>
+        @endforeach
+        @endif 
+    </div>
+
+</main>
+    {{-- <h1>Welcome to the blade</h1>
 
     @foreach ($posts as $post)
      {{-- <article class="{{$loop-> even ? 'foobar' : ''}}"> --}}
-        <h1>
-            <a href="/posts/{{ $post-> slug }}">
-                {{ $post->title}}
-            </a>
-        </h1>
+        {{-- <h1> --}}
+            {{-- <a href="/posts/{{ $post-> slug }}"> --}}
+                {{-- {{ $post->title}} --}}
+            {{-- </a> --}}
+        {{-- </h1> --}}
 
-        <p>
+        {{-- <p>
             by <a href="/authors/{{$post->author->username}}"> {{$post->author->name}}</a> in <a href="/categories/{{ $post->category->slug}}"> {{$post->category->name}}</a>
-        </p>
+        </p> --}}
 
-        <div>
+        {{-- <div>
            {{ $post->excerpt}}
         </div>
-     </article>
+     </article> --}}
 
-    @endforeach
+    {{-- @endforeach --}} 
 
     @endsection
